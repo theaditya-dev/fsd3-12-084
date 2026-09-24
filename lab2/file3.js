ef196417116ae66027385aba4b65f913f27b5f75
Aditya kasaudhan <ag4114365@gmail.com>
2026-09-24 13:41:02 +0530

    update lab2

--- lab2/file3.js
+++ lab2/file3.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {stat} from "fs/promises"
 const fstat = await stat ("file.js");
 console.log("file size", fstat.size , " bytes ");
@@ -7,13 +6,3 @@ console.log(`is folder: ${fstat.isDirectory()}`);
 console.log( `is syslink: ${stat.isSymboliclick()}`);
 console.log(`is created on : ${fstat.birthtime}`);
 console.log(`last Used: ${fstat.attime}`);
-=======
-import {stat} from "fs/promises"
-const fstat = await stat ("file.js");
-console.log("file size", fstat.size , " bytes ");
-console.log(`is file: ${fstat. isFie()}`);
-console.log(`is folder: ${fstat.isDirectory()}`);
-console.log( `is syslink: ${stat.isSymboliclick()}`);
-console.log(`is created on : ${fstat.birthtime}`);
-console.log(`last Used: ${fstat.attime}`);
->>>>>>> 38fdb2a (new commit)
--- lab2/file4.js
+++ lab2/file4.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { mkdir} from "fs/promises";
 //create a recursive directory 
 // await mkdir("uploads/resume",{recursive:true});
@@ -7,14 +6,4 @@ import { mkdir} from "fs/promises";
 // await mkdir("uploads/images");
 
 //reove directory 
-=======
-import { mkdir} from "fs/promises";
-//create a recursive directory 
-// await mkdir("uploads/resume",{recursive:true});
-
-//create a single directory 
-// await mkdir("uploads/images");
-
-//reove directory 
->>>>>>> 38fdb2a (new commit)
 await rm("uploads",{recursive:true });
